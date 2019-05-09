9d6ce66ba500e1ad1868b932b34532fded715d3e
kilamieaz <im.sultaan05@gmail.com>
2019-05-09 10:24:24 +0700

    make auth

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::get('/', function () {
+    return view('layouts.admin.master');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
