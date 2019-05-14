7a76e9e405d00e35b40befa17b0eecb63dfdbb6b
kilamieaz <im.sultaan05@gmail.com>
2019-05-14 15:51:43 +0700

    enum information & semester

---
+++ app/Enums/Information.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class Information extends Enum
+{
+    const Additional = 0;
+    const Mandatory = 1;
+}
---
+++ app/Enums/Semester.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class Semester extends Enum
+{
+    //ganjil
+    const Odd = 0;
+    //genap
+    const Even = 1;
+}
