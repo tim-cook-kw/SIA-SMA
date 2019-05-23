d8f26db0d3d0eb7e6bd164d8908448350ec85224
kilamieaz <im.sultaan05@gmail.com>
2019-05-23 10:01:22 +0700

    update user

---
+++ database/migrations/2019_05_21_023643_create_user_profiles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->index();
+            $table->string('username');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->text('address');
+            $table->string('phone_number');
+            $table->integer('gender');
+            $table->integer('religion');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+}
