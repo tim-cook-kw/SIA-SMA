8b7461393c8612e8b7e6d2d118fc6b33c25c314a
Sultan imam muttaqin <im.sultaan05@gmail.com>
2019-06-18 16:44:41 +0700

    repair master blade

--- resources/views/layouts/admin/master.blade.php
+++ resources/views/layouts/admin/master.blade.php
@@ -29,9 +29,9 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
-    {{-- <script src="assets/plugins/nprogress/nprogress.js"></script> --}}
+    <!-- {{-- <script src="assets/plugins/nprogress/nprogress.js"></script> --}} -->
     <script type="text/javascript" src="{{ asset('js/nprogress.js') }}"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script> -->
 </head>
 
 <body class="header-fixed sidebar-static sidebar-dark header-light" id="body">
