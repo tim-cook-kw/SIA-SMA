a9b8bd98b555957d681937c075aa871e2ec84371
Sultan imam muttaqin <im.sultaan05@gmail.com>
2019-06-18 11:06:02 +0700

    repair user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,32 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            // $table->bigIncrements('id');
+            // $table->string('email')->unique();
+            // $table->timestamp('email_verified_at')->nullable();
+            // $table->string('password');
+            // $table->unsignedBigInteger('role_id')->index()->nullable();
+            // $table->rememberToken();
+            // $table->timestamps();
             $table->bigIncrements('id');
+            $table->integer('id_parent');
+            $table->integer('id_class');
+            $table->unsignedBigInteger('role_id')->index()->nullable();
+            $table->integer('nis')->nullable();
+            $table->integer('nip')->nullable();
+            $table->string('position')->nullable();
+            $table->integer('tahun_masuk')->nullable();
+            $table->string('origin_of_school')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->unsignedBigInteger('role_id')->index()->nullable();
+            $table->string('name'); 
+            $table->string('address'); 
+            $table->string('birth_date'); 
+            $table->string('gender'); 
+            $table->integer('phone'); 
+            $table->integer('religion'); 
+            $table->string('image');
             $table->rememberToken();
             $table->timestamps();
         });
